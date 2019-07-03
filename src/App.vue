228c27ac35c3f6b39af8b84d09b7c55d21d1b27f
Gerardo May <cgme.may@gmail.com>
2019-07-02 21:04:22 -0500

    Added headers auth

--- src/App.vue
+++ src/App.vue
@@ -29,8 +29,7 @@
                                 {{ movie.overview }}
                             </v-card-text>
                             <v-card-actions>
-                                <v-btn flat color="orange">Share</v-btn>
-                                <v-btn flat color="orange">Explore</v-btn>
+                                <v-btn flat color="orange" :click="'addToFavorite(movie.id)'">add to favorite</v-btn>
                             </v-card-actions>
                         </v-card>
                     </router-link>
@@ -76,8 +75,8 @@
 
     const axios = require('axios');
     const apiUrl = 'http://tmdb-api-backend.test/api/';
-    const AUTH_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImE4ZjA5NDdkZGU2NDA2MmQ3NzU1NmEyMDVjODM2ZGUyMDEzMTMzZjVhODAyNDZhZGUyNTg4ZGY2MjMzYjkzMmUzODliMTdlYTAxZGM4OTk0In0.eyJhdWQiOiIxIiwianRpIjoiYThmMDk0N2RkZTY0MDYyZDc3NTU2YTIwNWM4MzZkZTIwMTMxMzNmNWE4MDI0NmFkZTI1ODhkZjYyMzNiOTMyZTM4OWIxN2VhMDFkYzg5OTQiLCJpYXQiOjE1NjIwMDIzNTAsIm5iZiI6MTU2MjAwMjM1MCwiZXhwIjoxNTkzNjI0NzUwLCJzdWIiOiIzIiwic2NvcGVzIjpbXX0.hSvIxrkVbuXIvZr9fre_rSrlpgMX1VS_Mh8pvPk88XRX-RJVf7ZHuSCwzN7D2A4MWn-TJjvd1Ok1WxHQ7pcrVo1LmiIymmTBTTHnR7Amf3W5lMfa7fMa-uYChwImxiRlWZnYwO7lxeu3o78lUjepn3iyxsa0Ur4ASHUfW-9Azm40nf0ONhdHLsWp46KnDUbajbAohwRgCQAGi_2eSdTDTGCSM8vEGb-WaMvJwu1L881wZUgaVH78UIhvTteneoyJ2Gm6Al1TcDe2eWnTljBs6wR42CEJmj3GIffBl8d-EvYppZAeiHbRP0zGFOAPVOOcIciy-3pKxYDmp9RdilIEeVaIpsl3KKHwRl3IKAbA_ZXfABQINC8kkdjpLxaEvPi-dzj03XiHcDnBQaIOk9dOt_Y8uyDHaBhpIvy8ERm4zBNNbaTCksgH6e7xUV6LZV1lc6zvtzB4WzdWQNVsMDl5XjfXpLp57UP8TwtQDJhgAbtKhytf53VGGWW6xcHQScZAlIlh3qOU2QH6c4xhB3xqBT6x_ZkpEChOK9iOf4183cNYwfH4jlMlq2MS6C9z_-L4Y0GtQRi15w8c2vUjq8tf1BpRNtsf26kOZXL2UAaQ23QQS4N_FOIipQ3IgsShSiNmxHzYeOMPzzUpS1lTaqWXc4TnBrXQf2yb92oAEp265-g';
-    axios.defaults.headers.common['Bearer'] = AUTH_TOKEN;
+    const AUTH_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjE5YWFlNjAyYWViOWRjMzk0MDc3OWVkNTZhY2RkODVmNjJiZGE2MmFhNDVhNjU4MmQ4YzMwZjk0NjAzNmM2MzQxYjMwYTZiNjA5MzZhZjljIn0.eyJhdWQiOiIyIiwianRpIjoiMTlhYWU2MDJhZWI5ZGMzOTQwNzc5ZWQ1NmFjZGQ4NWY2MmJkYTYyYWE0NWE2NTgyZDhjMzBmOTQ2MDM2YzYzNDFiMzBhNmI2MDkzNmFmOWMiLCJpYXQiOjE1NjIwNjg3MTQsIm5iZiI6MTU2MjA2ODcxNCwiZXhwIjoxNTkzNjkxMTE0LCJzdWIiOiIzIiwic2NvcGVzIjpbXX0.oMmDf8uGhP53PL6ujDmSAZ5k_WiSDJsW28JxdDNB8GdBqT88PXShb7SABekmrLRJM_SvhjQqkXdQ0TlTgJDGAKblBOe_UECMzTIjL0erBLf8RWBgsSSY6O_ysPvghKkg9j2H0Q7gRI7icbPFt8_Jrb2MYg8sNDTA47WgumjieaYWcrp_3VnTSJTJ-rdWpCS8dInfADGySCqJxs6yu1uerLPNK4iM4Q82VSt8ps1eS2lHL1RkPxp2h8i1jXqmx7ahPrIgd0byQxMnHaWfhBfHMlJNJ-B1lGE3t0BEhc_g0BvwJEQXvu-fsoZl0PZXnMvaG1_IqmKTOp3tNlE5uSOzNyPp1ZfFsZkPiL-Qf_4fUfvuMKDykwkS3oqBImjHBLiZ60fWFsEYfi4sBh5KZVo1eh1XsQzknLTMCWIaKUuoGi1ckCKNokUb4w4P6z7WgqgsBRcNwmA64rfEl6PPwaRbLmf3Og1P_2T2Z2M0_TeAiROXmYQ4W6-xnRwaPspJ1PMuXpitTTbBQv_-aizDsNVfZ8mxZtUvt_nwG9yNmRWRiE8lJHLCDdusN9qw0-2O0WAjZ2_LMwGlwWBt65fgl2KDT2EuJytd7fm0lKBI75mU2moOK03h9F-RW_xL0ou9lO3DrRat2iuC-v9AhQ7ZEJ3pISwfrPBmK3yoz1cNItY4BfE';
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + AUTH_TOKEN;
     axios.defaults.headers.common['Accept'] = 'application/json';
     axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
@@ -100,6 +99,11 @@
                 .finally(function () {
                     // always executed
                 });
+        },
+        methods : {
+            addToFavorite: function(movieId) {
+                console.log(movieId)
+            }
         },
         data() {
             return {
